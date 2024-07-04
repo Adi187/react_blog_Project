aa5d3d74c144de978deffcb95802f5077f4b135d
Aditya sharma <adityaraysharma@gmail.com>
2024-07-05 00:04:57 +0530

    added tiny mce api key

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,7 +3,8 @@ const conf={
     appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),//appwriteDatabaseId
     appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appWriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appWriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteEditorKey:String(import.meta.env.VITE_APPWRITE_EDITOR_KEY)
 }
 
 export default conf
