7e877f337212d5f7a03f7ca93a58a58ce5009ea8
Aditya sharma <adityaraysharma@gmail.com>
2024-07-05 02:13:15 +0530

    update post-form

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -38,7 +38,7 @@ export default function Post() {
     
     return post ? (
         <div className="py-8">
-            <Container>
+            <Container className="text-white">
                 <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
                     <img
                         src={appwriteService.getFilePreview(post.featuredImage)}
